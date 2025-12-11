e2311c4d686a97a0486ead44f271b2f6f738caa7
2018wzh <wzh20070720@outlook.com>
2025-12-11 15:03:47 +0800

    添加项目文件。

---
+++ Source/UEMeshViewer/UEMeshViewer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEMeshViewer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEMeshViewer, "UEMeshViewer" );
